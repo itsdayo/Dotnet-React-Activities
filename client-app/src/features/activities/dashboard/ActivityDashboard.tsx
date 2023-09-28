94bbbb677dade83f7b4d4a807ef95d654892da0b
dayo.ogundipe <dayo.ogundipe@teamworxsecurity.com>
2023-09-28 15:09:17 -0700

    UI implementation. forms, pictures with semanticUI

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Grid, List } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForn from "../form/ActivityForm";
+
+interface Props {
+  activities: Activity[];
+  selectedActivity: Activity | undefined;
+  selectActivity: (id: string) => void;
+  cancelSelectedActivity: () => void;
+  editMode: boolean;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  handleSubmitActivity: (activity: Activity) => void;
+  handleDeleteActivity: (id: string) => void;
+}
+
+export default function ActivityDasboard({
+  activities,
+  selectedActivity,
+  selectActivity,
+  cancelSelectedActivity,
+  editMode,
+  openForm,
+  closeForm,
+  handleSubmitActivity,
+  handleDeleteActivity
+}: Props) {
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <List>
+          <ActivityList
+            activities={activities}
+            selectActivity={selectActivity}
+            handleDeleteActivity={handleDeleteActivity}
+          />
+        </List>
+      </Grid.Column>
+      <Grid.Column width="6">
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            cancelSelectedActivity={cancelSelectedActivity}
+            openForm={openForm}
+          />
+        )}
+        {editMode && (
+          <ActivityForn
+            closeForm={closeForm}
+            activity={selectedActivity}
+            handleSubmitActivity={handleSubmitActivity}
+          />
+        )}
+      </Grid.Column>
+    </Grid>
+  );
+}
