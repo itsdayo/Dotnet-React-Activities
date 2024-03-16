0659606c3b3cfbbadd2ae7e1f9ae7039bb1b814f
dayo.ogundipe <dayo.ogundipe@teamworxsecurity.com>
2024-03-15 21:31:26 -0700

    register password regex

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,7 +8,7 @@ namespace API.DTOs
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage= "Password must be complex")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$", ErrorMessage= "Password must be more than 7 characters, contain a specical character, digit, lowercase letter and uppercase letter")]
         public string Password { get; set; }
         [Required]
         public string DisplayName { get; set; }
